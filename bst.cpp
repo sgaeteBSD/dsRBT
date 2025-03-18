33a58d2e5c48265d484e606e965947c5f77652f4
unknown <sgaeteBSD@github.com>
2025-03-18 06:13:04 +0100

    more bst class work

--- bst.cpp
+++ bst.cpp
@@ -7,10 +7,49 @@ class bst {
 public:
   bst();
   ~bst();
-  int insert(const int &data);
-  int remove(const int &key);
-  int search(const int &key);
+  void insert(BTNode* &root, int &data);
+  int remove(int &key);
+  BTNode* search(int key);
   void print();
 private:
   BTNode* root;
+  BTNode* trueSearch(BTNode* node, int &key);
 };
+
+//constructor
+//destructor
+
+void bst::insert(BTNode* &root, int &data) {
+  if (!root) { //whenever the passed node is NULL, create it
+    BTNode* root = new BTNode;
+    root->setData(data);
+    root->setLeft(NULL);
+    root->setRight(NULL);
+  }
+  else if (root->getData() > data) {
+    insert(root->getLeft(), data);
+  }
+  else {
+    insert(root->getRight(), data);
+  }
+}
+
+BTNode* bst::search(int key) {
+  return trueSearch(root, key);
+}
+
+BTNode* bst::trueSearch(BTNode* root, int &key) {
+  if (!root) {
+    return NULL; //doesn't exist or empty tree
+  }
+  if (root->getData() == key) {
+    return root; //found
+  }
+
+  if (key < root->getData()) {
+    return search(root->getLeft(), key); //search left
+  }
+  else {
+    return search(root->getRight(), key); //search right
+  }
+}
